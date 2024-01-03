2dd203cece9e886ee367ff4d5c039cd83851c076
gabrielluizsf <gabrielluizads123@gmail.com>
2024-01-03 11:56:30 -0300

    Adicionando mais oráculos

--- price/price_test.go
+++ price/price_test.go
@@ -1,17 +1,23 @@
 package price
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/gabrielluizsf/donttesteverything/assert"
 )
 
-func TestValue(test *testing.T){
+func TestValue(test *testing.T) {
 	result := Value("s")
 	expected := 90.0
 	a := assert.New(test)
-	a.Equals(result,expected)
+	a.Equals(result, expected)
 	result = Value("n")
-	fmt.Println(result)
+	expected = 0
+	a.Equals(result, expected)
+	result = Value("no")
+	expected = 0
+	a.Equals(result, expected)
+	result = Value("y")
+	expected = 0
+	a.Equals(result,expected)
 }
