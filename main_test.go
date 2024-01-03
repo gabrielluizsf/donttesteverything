daddd5fda3f2a1f4c0f74c76d949d592e20f95a6
gabrielluizsf <gabrielluizads123@gmail.com>
2024-01-03 12:50:24 -0300

    identando o código

--- main_test.go
+++ main_test.go
@@ -1,33 +1,33 @@
 package main
 
-import "testing";
+import "testing"
 
-func TestGetTypeTriangle(test *testing.T){
-	checkExpectedResult := func(test *testing.T, result,expected int){
-		test.Helper();
+func TestGetTypeTriangle(test *testing.T) {
+	checkExpectedResult := func(test *testing.T, result, expected int) {
+		test.Helper()
 		if result != expected {
-			test.Errorf("Result: %d Expected: %d",result,expected)
+			test.Errorf("Result: %d Expected: %d", result, expected)
 		}
 	}
-	test.Run("Não é um triangulo", func(test *testing.T){
-		nãoéumtrianguloResult   := GetTypeTriangle(0,0,0);
-		nãoéumtrianguloExpected := 0;
-			checkExpectedResult(test,nãoéumtrianguloResult,nãoéumtrianguloExpected);	
-	});
+	test.Run("Não é um triangulo", func(test *testing.T) {
+		nãoéumtrianguloResult := GetTypeTriangle(0, 0, 0)
+		nãoéumtrianguloExpected := 0
+		checkExpectedResult(test, nãoéumtrianguloResult, nãoéumtrianguloExpected)
+	})
 
-	test.Run("É Escaleno", func(test *testing.T){
-		escalenoResult   := GetTypeTriangle(10,12,13);
-		escalenoExpected := 30;
-			checkExpectedResult(test,escalenoResult,escalenoExpected);	
-	});
-	test.Run("É Isósceles", func(test *testing.T){
-		isoscelesResult   := GetTypeTriangle(12,12,13);
-		isoscelesExpected := 20;
-			checkExpectedResult(test,isoscelesResult,isoscelesExpected);	
-	});
-	test.Run("É Equilátero", func(test *testing.T){
-		equilateroResult   := GetTypeTriangle(13,13,13);
-		equilateroExpected := 10;
-			checkExpectedResult(test,equilateroResult,equilateroExpected);	
-	});
+	test.Run("É Escaleno", func(test *testing.T) {
+		escalenoResult := GetTypeTriangle(10, 12, 13)
+		escalenoExpected := 30
+		checkExpectedResult(test, escalenoResult, escalenoExpected)
+	})
+	test.Run("É Isósceles", func(test *testing.T) {
+		isoscelesResult := GetTypeTriangle(12, 12, 13)
+		isoscelesExpected := 20
+		checkExpectedResult(test, isoscelesResult, isoscelesExpected)
+	})
+	test.Run("É Equilátero", func(test *testing.T) {
+		equilateroResult := GetTypeTriangle(13, 13, 13)
+		equilateroExpected := 10
+		checkExpectedResult(test, equilateroResult, equilateroExpected)
+	})
 }
