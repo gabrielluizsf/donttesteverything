f2ac492dfa71de55355b18e95ee557c14b1239f2
gabrielluizsf <gabrielluizads123@gmail.com>
2024-01-03 12:52:25 -0300

    Adicionando mais oráculos

--- triangle/triangle_test.go
+++ triangle/triangle_test.go
@@ -11,4 +11,13 @@ func TestCheckIsTriangle(test *testing.T) {
 	expected := true
 	a := assert.New(test)
 	a.Equals(result, expected)
+	result = CheckIsTriangle(10, 10, 10)
+	expected = true
+	a.Equals(result, expected)
+	result = CheckIsTriangle(0,0,0)
+	expected = false
+	a.Equals(result,expected)
+	result = CheckIsTriangle(10,10,13)
+	expected = true
+	a.Equals(result,expected)
 }
